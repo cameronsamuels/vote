2e8ecbc080b44b5ddd271ce537b802ee305648d3
Cameron <54650557+cameronsamuels@users.noreply.github.com>
2020-07-31 01:46:13 -0500

    Work in progress

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 // Voting method selection
-(function(){
+(function() {
   function selectVoteMethod() {
     var absentee = document.querySelector("#vote-by-mail").checked;
     var els = document.querySelectorAll(".absentee");
@@ -17,21 +17,60 @@
 })();
 
 
-// Voting date selection
+// Dropdown selections
 (function() {
-  function selectVoteDate(e) {
-    document.querySelector(".dropdown>div>div").textContent = e.target.textContent;
-    document.querySelector("#statement-date").textContent = e.target.textContent;
+  function createDropdownEvents(dropdown, action) {
+    var els = document.querySelectorAll("#" + dropdown + ">div>div>div");
+    for (let i = 0; i < els.length; i++)
+      els[i].addEventListener("click", action);
   }
-  var els = document.querySelectorAll(".dropdown>div>div>div");
-  for (let i = 0; i < els.length; i++)
-    els[i].addEventListener("click", selectVoteDate);
+  createDropdownEvents("select-date", function(e) {
+    document.querySelector("#select-date>div>div").textContent = e.target.textContent;
+    document.querySelector("#statement-date").textContent = e.target.textContent;
+  });
+  createDropdownEvents("select-id", function(e) {
+    document.querySelector("#select-id>div>div").textContent = e.target.textContent;
+    document.querySelector("#statement-id").textContent = e.target.textContent;
+  });
 })();
+
+
+// Voting place selection
+function loadGAPI() {
+  gapi.client.setApiKey("AIzaSyBNWmDwDM49_L2RmNyRVX6veBXneKEGNF4");
+  lookup('1263 Pacific Ave. Kansas City KS', renderResults);
+}
+
+function lookup(address, callback) {
+  /**
+  * Election ID for which to fetch voter info.
+  * @type {number}
+  */
+  var electionId = 2000;
+
+  /**
+  * Request object for given parameters.
+  * @type {gapi.client.HttpRequest}
+  */
+  var req = gapi.client.request({
+    "path" : "/civicinfo/v2/elections"
+    // "path" : "/civicinfo/v2/voterinfo",
+    // "params" : {"electionId" : electionId, "address" : address}
+  });
+
+  req.execute(callback);
+}
+
+function renderResults(response, rawResponse) {
+  console.log(response);
+}
+
 
 // Print statement
 document.querySelector("#print-statement").addEventListener("click", function() {
   window.print();
 });
+
 
 // Add statement to calendar
 function updateAddToCalendar() {
