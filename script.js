776940a8f87ad683b9ff8eb0e00e88570c6aa6be
Cameron <54650557+cameronsamuels@users.noreply.github.com>
2020-08-05 00:04:45 -0500

    Work in progress

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Code by Cameron Samuels
 
 
+// Countdown
+(function() {
+  var el = document.querySelector("#countdown");
+  var d = new Date("October 5, 2020 11:59:59") - new Date();
+  el.textContent = "The deadline to register to vote in Texas is in " + Math.floor(d / 86400000) + " days";
+})();
+
+
 // Voting method selection
 (function() {
   function selectVoteMethod() {
@@ -24,14 +32,31 @@
     for (let i = 0; i < els.length; i++)
       els[i].addEventListener("click", action);
   }
-  createDropdownEvents("select-date", function(e) {
-    document.querySelector("#select-date>div>div").textContent = e.target.textContent;
-    document.querySelector("#statement-date").textContent = e.target.textContent;
-  });
   createDropdownEvents("select-id", function(e) {
     document.querySelector("#select-id>div>div").textContent = e.target.textContent;
     document.querySelector("#statement-id").textContent = e.target.textContent;
   });
+})();
+
+
+// Voting date selection
+(function() {
+  var drop = document.querySelector("#select-date>div>div:nth-of-type(2)");
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var dates = ["13", "14", "15", "16", "18"]; // TODO
+  for (let i = 0; i < dates.length; i++) {
+    var date = new Date("October " + dates[i] + ", 2020 19:00:00");
+    if (new Date() > date)
+      continue;
+    var str = days[date.getDay()] + ", October " + date.getDate();
+    var el = document.createElement("div");
+    el.textContent = str;
+    el.addEventListener("click", function(e) {
+      document.querySelector("#select-date>div>div").textContent = e.target.textContent;
+      document.querySelector("#statement-date").textContent = e.target.textContent;
+    });
+    drop.appendChild(el);
+  }
 })();
 
 
@@ -42,27 +67,26 @@ function loadGAPI() {
 }
 
 function lookup(address, callback) {
-  /**
-  * Election ID for which to fetch voter info.
-  * @type {number}
-  */
   var electionId = 2000;
-
-  /**
-  * Request object for given parameters.
-  * @type {gapi.client.HttpRequest}
-  */
   var req = gapi.client.request({
-    "path" : "/civicinfo/v2/elections"
-    // "path" : "/civicinfo/v2/voterinfo",
-    // "params" : {"electionId" : electionId, "address" : address}
+    // "path" : "/civicinfo/v2/elections"
+    "path" : "/civicinfo/v2/voterinfo",
+    "params" : {"electionId" : electionId, "address" : address}
   });
 
   req.execute(callback);
 }
 
 function renderResults(response, rawResponse) {
-  console.log(response);
+  var polls = response.pollingLocations;
+  for (let i = 0; i < polls.length; i++) {
+    var address = Object.values(polls[i].address);
+    var filtered = address.filter(function(el) {
+      return el != "";
+    }).join(", ");
+    console.log(filtered);
+    console.log(polls[i].pollingHours);
+  }
 }
 
 
