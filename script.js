b5817ed757ab7e25bed3be42bdae9ab5195a9868
Cameron <54650557+cameronsamuels@users.noreply.github.com>
2020-08-12 13:21:14 -0500

    Work in progress

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 })();
 
 
-// Checkbox
+// Large checkboxes
 (function() {
   var els = document.querySelectorAll("#checklist input[type='checkbox']");
   window.checkChange = function(e) {
@@ -110,75 +110,26 @@ document.querySelector("#show-details").addEventListener("click", function() {
 
 // Dropdown selections
 (function() {
-  function createDropdownEvents(dropdown, action) {
-    var els = document.querySelectorAll("#" + dropdown + ">div>div>div");
-    for (let i = 0; i < els.length; i++) {
-      els[i].addEventListener("click", action);
-      els[i].addEventListener("click", function(e) {
-        var el = e.target.parentElement.parentElement.parentElement.parentElement.querySelector("input[type='checkbox']");
-        el.checked = true;
-        localStorage[el.id] = true;
-        checkChange();
-      });
+  function dropdownEvent(e) {
+    var id;
+    if (e.type == "click") {
+      e.target.parentElement.parentElement.querySelector("div[contenteditable]").textContent = e.target.textContent;
+      id = e.target.parentElement.parentElement.parentElement.id;
+      e.target.parentElement.parentElement.parentElement.parentElement.querySelector("input[type='checkbox']").checked = true;
+      checkChange();
     }
-    document.querySelector("#" + dropdown + ">div>div").addEventListener("input", action);
+    else if (e.type == "input")
+      id = e.target.parentElement.parentElement.id;
+    document.querySelector(id.replace("select", "#statement")).textContent = e.target.textContent;
+    localStorage[id] = e.target.textContent;
   }
-  createDropdownEvents("select-id", function(e) {
-    document.querySelector("#select-id>div>div").textContent = e.target.textContent;
-    document.querySelector("#statement-id").textContent = e.target.textContent;
-    localStorage["select-id"] = e.target.textContent;
-  });
-  createDropdownEvents("select-transportation", function(e) {
-    document.querySelector("#select-transportation>div>div").textContent = e.target.textContent;
-    document.querySelector("#statement-transportation").textContent = e.target.textContent;
-    localStorage["select-transportation"] = e.target.textContent;
-  });
-  createDropdownEvents("select-time", function(e) {
-    document.querySelector("#select-time>div>div").textContent = e.target.textContent;
-    document.querySelector("#statement-time").textContent = e.target.textContent;
-    localStorage["select-time"] = e.target.textContent;
-  });
-  createDropdownEvents("select-location", function(e) {
-    document.querySelector("#select-location>div>div").textContent = e.target.textContent;
-    document.querySelector("#statement-location").textContent = e.target.textContent;
-    localStorage["select-location"] = e.target.textContent;
-  });
-  createDropdownEvents("select-date", function(e) {
-    document.querySelector("#select-date>div>div").textContent = e.target.textContent;
-    document.querySelector("#statement-date").textContent = e.target.textContent;
-    localStorage["select-date"] = e.target.textContent;
+  ["id", "transportation", "time", "location", "date"].forEach(function(i) {
+    var els = document.querySelectorAll("#select-" + i + ">div>div>div");
+    for (let j = 0; j < els.length; j++)
+      els[j].addEventListener("click", dropdownEvent);
+    document.querySelector("#select-" + i + ">div>div").addEventListener("input", dropdownEvent);
   });
 })();
-
-
-// Voting place selection
-// function loadGAPI() {
-//   gapi.client.setApiKey("AIzaSyBNWmDwDM49_L2RmNyRVX6veBXneKEGNF4");
-//   lookup('1263 Pacific Ave. Kansas City KS', renderResults);
-// }
-
-// function lookup(address, callback) {
-//   var electionId = 2000;
-//   var req = gapi.client.request({
-//     // "path" : "/civicinfo/v2/elections"
-//     "path" : "/civicinfo/v2/voterinfo",
-//     "params" : {"electionId" : electionId, "address" : address}
-//   });
-
-//   req.execute(callback);
-// }
-
-// function renderResults(response, rawResponse) {
-//   var polls = response.pollingLocations;
-//   for (let i = 0; i < polls.length; i++) {
-//     var address = Object.values(polls[i].address);
-//     var filtered = address.filter(function(el) {
-//       return el != "";
-//     }).join(", ");
-//     console.log(filtered);
-//     console.log(polls[i].pollingHours);
-//   }
-// }
 
 
 // Passting in text
